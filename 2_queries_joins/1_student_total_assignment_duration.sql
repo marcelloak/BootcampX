6db14e0560c61f1d3caf8d973e14247ddb32bd12
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-12 11:29:48 -0600

    Created 2_queries_joins

---
+++ 2_queries_joins/1_student_total_assignment_duration.sql
@@ -0,0 +1,3 @@
+SELECT SUM(duration) AS total_duration
+FROM students JOIN assignment_submissions ON students.id = student_id
+WHERE name = 'Ibrahim Schimmel';
---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(duration) AS total_duration
+FROM students JOIN assignment_submissions ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
