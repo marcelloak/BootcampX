05c0b054c180130e0d730f1fc4766c82fcab52d0
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-12 12:59:37 -0600

    Completed 3_queries_group-by

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) AS total_submissions
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = student_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
